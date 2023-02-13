7b73caffdb57f52700d4accfd1c0d94a2a879b07
Toznyy <anthony.cormier29860@gmail.com>
2023-02-13 11:31:08 +0000

    ajout de la suite

---
+++ services/Router.php
@@ -0,0 +1,37 @@
+<?php
+
+class Router {
+    
+    private UserController $userController;
+    private CategoryController $categoryController;
+    private SalonController $salonController;
+    private MessageController $messageController;
+    
+    public function __construct() {
+        $this->userController = new UserController();
+        $this->categoryController = new CategoryController();
+        $this->salonController = new SalonController();
+        $this->messageController = new MessageController();
+    }
+    
+    public function checkRoute(string $route) : void {
+        
+        if ($route === "users") {
+            $this->route -> UserController::index;
+        }
+        
+        else if ($route === "user-create") {
+            $this->route -> UserController::create;
+        }
+        
+        else if ($route === "user-edit") {
+            $this->route -> UserController::edit;
+        }
+        
+        else {
+            $this->route -> UserController::index;
+        }
+    }
+}
+
+?>
